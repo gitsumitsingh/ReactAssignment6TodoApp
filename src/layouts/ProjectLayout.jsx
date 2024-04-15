834ad07ffc951618da0923870a77df12f4fdc5c8
SumitSPDC <sumit.singh@powerweave.com>
2024-04-15 19:54:01 +0530

    first commit

---
+++ src/layouts/ProjectLayout.jsx
@@ -0,0 +1,19 @@
+import {NavLink, Outlet} from 'react-router-dom'
+
+function ProjectMenu() {
+  return (
+    <div className='projects'>
+        <div className="flex">
+            <span className='subtitle'>Projects</span>
+            <NavLink className="project-link" to="todo">To Do List</NavLink>
+            <NavLink className="project-link" to="townhouse">Townhouse</NavLink>
+            <NavLink className="project-link" to="condominium">Condominium</NavLink>
+        </div>
+        <div className="project-list">
+            <Outlet/>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectMenu
